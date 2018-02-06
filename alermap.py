ffbc23e742dba5dba84785db320a3250f29f44c4
aracelivaladez <aracely_valades@hotmail.com>
2018-02-06 12:50:18 -0600

    correccion al codigo

--- alermap.py
+++ alermap.py
@@ -2,14 +2,14 @@ from ftplib import FTP #Libreria utilizada para conectarse a un servidor FTP y o
 import os #Libreria utilizada para crear carpetas de almacenamiento
 import requests #Libreria utilizada para obtener el url
 import sys #Libreria utilizada para obtener el codigo obtenido
-from api import clave #Importa la api
+from api import claves #Importa la api
 
 def fecha_usr(ip): #Obtener fecha del url
 
     conexion = FTP(ip) #Nombre del servidor
     conexion.login(clave.usr, clave.pwd) #Usuario y contrasena del servidor
     fecha = [] 
-    ftp.dir(fecha.append) #Se almacena toda la informacion que se encuentra en el directorio actual dentro del arreglo
+    conexion.dir(fecha.append) #Se almacena toda la informacion que se encuentra en el directorio actual dentro del arreglo
     fecha = fecha[-1].split()[-1] #Se toma el ultimo valor del arreglo, se separa la cadena en un arreglo dividido por espacios y se toma el ultimo valor.
     return fecha # Se devuelve el valor obtenido
     
@@ -36,37 +36,36 @@ def cinco_dias(fecha): #Obtener cuatro dias posteriores a la fecha obtenida
                 dias.append('{:04d}-01-{:02d}'.format(ano + 1, n - (dias_mes - dia)))
     return dias
 
-def descargar_datos(fecha):    	
+def descarga_datos(fecha):
     try:
-        conexion = FTP(ip)
-        conexion.login(clave.usr, clave.pwd)
-        print("conexion exitosa")
+        conexion=FTP(clave.ip);
+        conexion.login(clave.usr,clave.pwd)
+        print("Conexion exitosa")
+
+        conexion.cwd('{}'.format(fecha))
+
+        if os.path.exists('data'):
+            os.chdir('data')
+        else:
+            os.mkdir('data')
+            os.chdir('data') 
+        if os.path.exists('{}'.format(fecha)):
+            for i in range(1,6):
+                conexion.retrbinary('RETR d{}.txt'.format(i),open('{}/d{}.txt'.format(fecha, i),'wb').write)
+        else:
+            os.mkdir('{}'.format(fecha))
+            for i in range(1,6):
+                conexion.retrbinary('RETR d{}.txt'.format(i),open('{}/d{}.txt'.format(fecha, i), 'wb').write)
+        conexion.quit()
+        os.chdir('..')
+
     except ValueError:
-         print("conexion fallida")
-
-    conexion.cwd('{}'.format(fecha)) #Ingresa a una carpeta dentro del servidor
-	
-    if os.path.exists('datos'):
-    #Verifica si la carpeta datos existe (donde se almacenaran los documentos a descargar)
-		os.chdir('datos') #Accede a la carpeta datos
-	else:
-		os.mkdir('datos') #Crea la carpeta datos 
-		os.chdir('datos') #Accede a la carpeta datos
-	if os.path.exists('{}'.format(fecha)): 
-		for i in range(1, 6): #Ciclo que realiza 5 veces el proceso incrementando su valor 1
-		    ftp.retrbinary('RETR d{}.txt'.format(i),open('{}/d{}.txt'.format(fecha, i),'wb').write) #Descarga los documentos
-	else:
-		os.mkdir('{}'.format(fecha)) #Crea la carpeta fecha donde se almacenaran los documentos
-		for i in range(1, 6): #Ciclo que realiza 5 veces el proceso incrementando su valor 1
-           ftp.retrbinary('RETR d{}.txt'.format(i),open('{}/d{}.txt'.format(fecha, i),'wb').write) #Descarga los documentos
-
-	ftp.quit()
-	os.chdir('..') #Sale de la carpeta datos al directorio raiz
-
+        print("Conexion fallida")
 
 clave=claves()
 fecha=fecha_usr(clave.ip)
 print(fecha)
 print(cinco_dias(fecha))
-descargar_datos(fecha)
+descarga_datos(fecha)
+
 
