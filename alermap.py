0194b7c8e6b23a118c946d71f184120bf89ef47e
jorgemauricio <jorge.ernesto.mauricio@gmail.com>
2018-02-08 12:38:45 -0600

    solve conflicts 2

--- alermap.py
+++ alermap.py
@@ -1,35 +1,27 @@
-<<<<<<< HEAD
-from ftplib import FTP #Libreria utilizada para conectarse a un servidor FTP y obtener informacion
-import os #Libreria utilizada para crear carpetas de almacenamiento
-import requests #Libreria utilizada para obtener el url
-import sys #Libreria utilizada para obtener el codigo obtenido
-from api import claves #Importa la api
-=======
-from ftplib import FTP 
-import os 
-import sys 
-from api import claves 
+from ftplib import FTP
+import os
+import sys
+from api import claves
 
 
 def main():
     print(fecha)
     cinco_dias(fecha)
     descarga_datos(fecha)
->>>>>>> chely2
 
 def fecha_usr(ip): #Obtener fecha del url
 
     conexion = FTP(ip) #Nombre del servidor
     conexion.login(clave.usr, clave.pwd) #Usuario y contrasena del servidor
-    fecha = [] 
+    fecha = []
     conexion.dir(fecha.append) #Se almacena toda la informacion que se encuentra en el directorio actual dentro del arreglo
     fecha = fecha[-1].split()[-1] #Se toma el ultimo valor del arreglo, se separa la cadena en un arreglo dividido por espacios y se toma el ultimo valor.
     return fecha # Se devuelve el valor obtenido
-    
+
 
 def cinco_dias(fecha): #Obtener cuatro dias posteriores a la fecha obtenida
-    ano, mes, dia = (int(n) for n in fecha.split("-")) #Almacenamos cada dato correspondiente dividiendolo por un (-) 
-    if mes in (1, 3, 5, 7, 8, 10, 12): #Validacion de fecha 
+    ano, mes, dia = (int(n) for n in fecha.split("-")) #Almacenamos cada dato correspondiente dividiendolo por un (-)
+    if mes in (1, 3, 5, 7, 8, 10, 12): #Validacion de fecha
         dias_mes = 31
     elif mes == 2:
         if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0): #Si el mes se bisiesto
@@ -38,7 +30,7 @@ def cinco_dias(fecha): #Obtener cuatro dias posteriores a la fecha obtenida
             dias_mes = 28
     elif mes in (4, 6, 9, 11):
         dias_mes = 30
-    dias = [] 
+    dias = []
     for n in range(0, 5): #Ciclo utilizado para almacenar los 5 dias
         if dia + n <= dias_mes:
             dias.append('{:04d}-{:02d}-{:02d}'.format(ano, mes, dia + n)) #'{:04d}-{:02d}-{:02d}' - Formato para la fecha
@@ -61,41 +53,26 @@ def descarga_datos(fecha):
             os.chdir('data')
         else:
             os.mkdir('data')
-            os.chdir('data') 
+            os.chdir('data')
         if os.path.exists('{}'.format(fecha)):
             for i in range(1,6):
-<<<<<<< HEAD
-=======
                 print("Descargando archivo d{}.txt".format(i))
->>>>>>> chely2
                 conexion.retrbinary('RETR d{}.txt'.format(i),open('{}/d{}.txt'.format(fecha, i),'wb').write)
         else:
             os.mkdir('{}'.format(fecha))
             for i in range(1,6):
-<<<<<<< HEAD
-                conexion.retrbinary('RETR d{}.txt'.format(i),open('{}/d{}.txt'.format(fecha, i), 'wb').write)
-        conexion.quit()
-        os.chdir('..')
-
-=======
                 print("Descargando archivo d{}.txt".format(i))
                 conexion.retrbinary('RETR d{}.txt'.format(i),open('{}/d{}.txt'.format(fecha, i), 'wb').write)
         conexion.quit()
         os.chdir('..')
->>>>>>> chely2
     except ValueError:
         print("Conexion fallida")
 
 clave=claves()
 fecha=fecha_usr(clave.ip)
-<<<<<<< HEAD
 print(fecha)
 print(cinco_dias(fecha))
 descarga_datos(fecha)
 
-
-=======
-
 if __name__=="__main__":
-    main() 
->>>>>>> chely2
+    main()
